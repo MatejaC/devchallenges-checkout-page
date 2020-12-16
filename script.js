ee7cc127db24d1327461fe1cd347ea50792e96c5
Mateja Cimpersak <matejacimpersak@gmail.com>
2020-12-16 14:52:50 +0100

    adding js scrpit for incrementing numbers for purchased items

---
+++ script.js
@@ -0,0 +1,16 @@
+function incrementor(event) {
+    console.log(event);
+    let [inputElementId, action] = ["for", "action"].map(attr => event.target.getAttribute(attr))
+
+    if (inputElementId == null && action == null) {
+        [inputElementId, action] = ["for", "action"].map(attr => event.target.parentNode.getAttribute(attr))
+    }
+
+    let inputElement = document.getElementById(inputElementId)
+
+    console.log(inputElement);
+
+    if (action == 'add') {
+        inputElement.value = +inputElement.value + 1
+    } else if (action == 'remove') inputElement.value = inputElement.value - 1
+}
